de068b43e3584d02d2c47b8cf55e45e2a72dcc1e
Lucas Pedrosa <pedrosa.lucas@hotmail.com>
2020-07-29 14:31:18 -0300

    Fim da terceira aula

--- src/pages/Register/Category/index.js
+++ src/pages/Register/Category/index.js
@@ -1,11 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import FormField from '../../../components/FormField';
 
 function CategoryRegister() {
+    const initialsValues = {
+        name: '',
+        description: '',
+        color: ''
+    };
+    const [categories, setCategories] = useState([]);
+    const [values, setValues] = useState(initialsValues);
+
+    function setValue(key, value) {
+        setValues({
+            ...values,
+            [key]: value
+        })
+    }
+
+    function handleChange(event) {
+        setValue(
+            event.target.getAttribute('name'),
+            event.target.value
+        );
+    }
+
     return (
         <PageDefault>
-            <h1>Cadastro de Categoria</h1>
+            <h1>Cadastro de Categoria: {values.name}</h1>
+
+            <form onSubmit={(event) => {
+                event.preventDefault();
+                setCategories([
+                    ...categories,
+                    values
+                ]);
+                setValues(initialsValues);
+            }}>
+                <FormField 
+                    label="Nome da Categoria"
+                    type="text"
+                    name="name"
+                    value={values.name}
+                    onChange={handleChange}
+                />
+
+                <FormField 
+                    label="Descrição"
+                    type="textarea"
+                    name="description"
+                    value={values.description}
+                    onChange={handleChange}
+                />
+
+                <FormField 
+                    label="Cor"
+                    type="color"
+                    name="color"
+                    value={values.color}
+                    onChange={handleChange}
+                />
+
+                <button>
+                    Cadastrar
+                </button>
+            </form>
+
+            <ul>{categories.map((item, index) => {
+                return (
+                    <li key={`${item}${index}`}>
+                        {item.name}
+                    </li>
+                );
+            })}</ul>
             
             <Link to="/register/category">
                 Cadastrar Nova Categoria
